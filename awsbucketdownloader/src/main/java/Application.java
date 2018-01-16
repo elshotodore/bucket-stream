a87f57613c2b4903e0db6897f36d5e0db8493640
FrankVogel666 <frank.vogel@awin.com>
2018-01-16 15:50:35 +0100

    added helper to downloader

--- awsbucketdownloader/src/main/java/Application.java
+++ awsbucketdownloader/src/main/java/Application.java
@@ -1,6 +1,6 @@
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.AccessControlList;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
@@ -9,10 +9,11 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
-                .withForceGlobalBucketAccessEnabled(true)
-                //.withRegion(Regions.EU_WEST_1)
+                //.withForceGlobalBucketAccessEnabled(true)
+                //.withRegion(Regions.US_WEST_2)
                 .build();
-        String bucketName = "pilot";
+        final List<String> buckets = Helper.readBucketList("../buckets.log", true);
+        String bucketName = buckets.get(0);
 
         ObjectListing ol = s3.listObjects(bucketName);
         List<S3ObjectSummary> objects = ol.getObjectSummaries();
